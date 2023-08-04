5cae88d27f2a13bfce966d570a7d5f3bc25753fc
Teemu Taivainen <teemu.taivainen@aalto.fi>
2023-08-04 14:34:31 +0300

    10.3 done

---
+++ src/Main.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+import RepositoryList from './components/RepositoryList'
+
+const Main = () => (
+  <View>
+    <Text>Rate Repository Application</Text>
+    <RepositoryList />
+  </View>
+)
+
+export default Main
---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,17 @@
+import { Text, View } from 'react-native'
+
+const reposiToryItem = ({ repository }) => {
+  return (
+    <View>
+      <Text>Full name: {repository.fullName}</Text>
+      <Text>Description: {repository.description}</Text>
+      <Text>Language: {repository.language}</Text>
+      <Text>Stars: {repository.stargazersCount}</Text>
+      <Text>Forks: {repository.forksCount}</Text>
+      <Text>Reviews: {repository.reviewCount}</Text>
+      <Text>Rating: {repository.ratingAverage}</Text>
+    </View>
+  )
+}
+
+export default reposiToryItem
