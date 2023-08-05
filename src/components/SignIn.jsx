a7b9fb7bc3cbc1a603f3e95cbbd6d7a86ac9d6fe
Teemu Taivainen <teemu.taivainen@aalto.fi>
2023-08-05 15:47:03 +0300

    10.8 logic done

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,30 @@
-import { Text } from 'react-native'
+import { View, Pressable } from 'react-native'
+import { Formik } from 'formik'
+import FormikTextInput from './FormikTextInput'
+import Text from './Text'
+
+const onSubmit = (values) => {
+  console.log(values)
+}
+
+const SignInForm = ({ onSubmit }) => {
+  return (
+    <View>
+      <FormikTextInput name='username' placeholder='Username' />
+      <FormikTextInput name='password' placeholder='Password' secureTextEntry />
+      <Pressable onPress={onSubmit}>
+        <Text>Sign in</Text>
+      </Pressable>
+    </View>
+  )
+}
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>
+  return (
+    <Formik initialValues={{ username: '', password: '' }} onSubmit={onSubmit}>
+      {(props) => <SignInForm onSubmit={props.handleSubmit} />}
+    </Formik>
+  )
 }
 
 export default SignIn
