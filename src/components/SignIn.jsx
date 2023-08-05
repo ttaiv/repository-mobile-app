61264a493d9503050ee82d7c706aa79e1623783a
Teemu Taivainen <teemu.taivainen@aalto.fi>
2023-08-05 16:15:43 +0300

    10.8 styling done

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,23 @@
-import { View, Pressable } from 'react-native'
+import { View, Pressable, StyleSheet } from 'react-native'
 import { Formik } from 'formik'
 import FormikTextInput from './FormikTextInput'
 import Text from './Text'
+
+const styles = StyleSheet.create({
+  signinButton: {
+    backgroundColor: '#0366d6',
+    color: 'white',
+    padding: 10,
+    borderWidth: 1,
+    marginTop: 5,
+    fontSize: 12,
+    margin: 20,
+  },
+  signinButtonText: {
+    color: 'white',
+    alignSelf: 'center'
+  }
+})
 
 const onSubmit = (values) => {
   console.log(values)
@@ -12,8 +28,8 @@ const SignInForm = ({ onSubmit }) => {
     <View>
       <FormikTextInput name='username' placeholder='Username' />
       <FormikTextInput name='password' placeholder='Password' secureTextEntry />
-      <Pressable onPress={onSubmit}>
-        <Text>Sign in</Text>
+      <Pressable style={styles.signinButton} onPress={onSubmit}>
+        <Text style={styles.signinButtonText}>Sign in</Text>
       </Pressable>
     </View>
   )
--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,9 +1,19 @@
 import { TextInput as NativeTextInput, StyleSheet } from 'react-native'
+import theme from '../theme'
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  textInput: {
+    borderColor: theme.colors.textSecondary,
+    borderWidth: 1,
+    padding: 10,
+    margin: 20,
+    marginBottom: 10,
+    marginTop: 10,
+  },
+})
 
 const TextInput = ({ style, error, ...props }) => {
-  const textInputStyle = [style]
+  const textInputStyle = [styles.textInput, style]
 
   return <NativeTextInput style={textInputStyle} {...props} />
 }
